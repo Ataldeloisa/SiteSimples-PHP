e9d156e964434afb287560696a8890f0da241521
Ataldeloisa <140725430+Ataldeloisa@users.noreply.github.com>
2026-08-25 10:29:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FOOOOOOOOOOOOOOOOOOOODASE</title>
+    <title>Aula de fdp</title>
 
     <!-- Importa Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtEzwO5IkR0UeSAfVyutY5xpMRWsFZ88gDAe_Uz6Ld1gk0BDbiAl4OKJco&s=10">
 
 </head>
 <body>
